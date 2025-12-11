7acbf550e10cbcfc5b814daf19283d614bfec80e
Your Name <Abhayprabodh01@gmail.com>
2025-12-11 23:14:31 +0200

    Extra task: Added farewell function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def greet(name):
     return f"Hello, {name}! This is Homework 11."
 
+def farewell(name):
+    return f"Goodbye, {name}! Hope to see you soon."
+
 if __name__ == "__main__":
     print(greet("Abhay"))
+    print(farewell("Abhay"))
